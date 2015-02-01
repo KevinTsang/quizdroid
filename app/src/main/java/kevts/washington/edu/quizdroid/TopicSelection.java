9d285875bf0c69672130b882b579d35c124e921a
KevinTsang <kevin@tsangfamily.us>
2015-02-01 00:11:04 -0800

    First commit, created basic interface layouts

---
+++ app/src/main/java/kevts/washington/edu/quizdroid/TopicSelection.java
@@ -0,0 +1,18 @@
+package kevts.washington.edu.quizdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class TopicSelection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_selection);
+    }
+
+
+}
