48c894426f9f649d7294da833757dcebd420c419
KevinTsang <kevin@tsangfamily.us>
2015-02-03 12:30:28 -0800

    Completed QuestionPage and AnswerSummary functionality

--- app/src/main/java/kevts/washington/edu/quizdroid/QuestionPage.java
+++ app/src/main/java/kevts/washington/edu/quizdroid/QuestionPage.java
@@ -1,19 +1,21 @@
 package kevts.washington.edu.quizdroid;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
-public class QuestionPage extends Activity {
+public class QuestionPage extends Activity implements OnClickListener {
 
+    private int currentQuestion;
     private int nextQuestionIndex;
-
     private int selectedAnswer;
 
     @Override
@@ -22,7 +24,7 @@ public class QuestionPage extends Activity {
         setContentView(R.layout.activity_question_page);
         Intent parentCall = getIntent();
         String topic = parentCall.getStringExtra("topic");
-        int currentQuestion = parentCall.getIntExtra("questionNumber", 0);
+        final int currentQuestion = parentCall.getIntExtra("questionNumber", 0);
         switch (topic) {
             case "math":
                 QuestionAnswer[] mathQuestions = QuizInit.initializeMath();
@@ -94,32 +96,40 @@ public class QuestionPage extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(QuestionPage.this, AnswerSummary.class);
+                int answersCorrect = getIntent().getIntExtra("answersCorrect", -1);
+                if (answersCorrect != -1) {
+                    intent.putExtra("answersCorrect", answersCorrect);
+                }
                 intent.putExtra("selectedAnswer", selectedAnswer);
+                intent.putExtra("previousQuestion", currentQuestion);
                 intent.putExtra("currentQuestion", nextQuestionIndex);
-            }
-        });
-
-
-        final RadioGroup choices = (RadioGroup)findViewById(R.id.choices);
-        choices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                submit.setVisibility(View.VISIBLE);
-                selectedAnswer = choices.getCheckedRadioButtonId();
+                String topic = getIntent().getStringExtra("topic");
+                intent.putExtra("topic", topic);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
     public void populate(QuestionAnswer[] array, int i) {
+        currentQuestion = i;
         TextView question = (TextView)findViewById(R.id.question);
         question.setText(array[i].getQuestion());
-        for (String s : array[i].getAnswers()) {
+        for (int x = 0; x < array.length; x++) {
             RadioGroup choices = (RadioGroup)findViewById(R.id.choices);
             RadioButton choice = new RadioButton(this);
-            choice.setText(s);
-            choice.setTag("radioButtons");
-            choices.addView(choice, i);
+            choice.setText(array[i].getAnswers()[x]);
+            choice.setId(x);
+            choice.setOnClickListener(this);
+            choices.addView(choice, x);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button submit = (Button)findViewById(R.id.submit);
+        submit.setVisibility(View.VISIBLE);
+        RadioGroup choices = (RadioGroup)findViewById(R.id.choices);
+        selectedAnswer = choices.getCheckedRadioButtonId();
     }
 
 }
